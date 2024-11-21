d6303e982ac67a15b40d4ada94eb675f478dfb2f
RML1812 <raditya.lacavi@gmail.com>
2024-11-21 12:36:15 +0700

    fix: namefile change for better usability

--- text_responses.py
+++
@@ -1,3 +0,0 @@
-start_rp = "🤖 Selamat datang :) \n\nBot ini menekankan keamanan dan kenyamanan kalian 🔒, 🔍 cari tau lebih lanjut dengan mengirimkan /help \n\nMulai pencarian obrolan dengan mengirimkan /chat"
-handle_chat_paired = "🤖 Kamu sudah di dalam chat"
-handle_search_searching = "🤖 Kamu masih dalam proses pencarian"
